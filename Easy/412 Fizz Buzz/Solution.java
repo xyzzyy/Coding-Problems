41fac77621fde84848d55dfde974095cf5e46db9
mariotosdig <ajmackenzie@mta.ca>
2023-01-05 23:54:50 -0400

    added java solution for E-412

---
+++ Easy/412 Fizz Buzz/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/fizz-buzz/submissions/689739994/
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> answer = new ArrayList<String>();
+        for (int i=1; i<n+1; i++){
+            if (i%3==0 && i%5==0){
+                answer.add("FizzBuzz");
+            } else if (i%3==0){
+                answer.add("Fizz");
+            } else if (i%5==0){
+                answer.add("Buzz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+}
