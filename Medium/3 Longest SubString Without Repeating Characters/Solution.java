25e28ff4938804fc6fcaefe95397ebf86014a93a
mariotosdig <ajmackenzie@mta.ca>
2023-01-11 22:31:46 -0400

    added new problem base file

---
+++ Medium/3 Longest SubString Without Repeating Characters/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        for (int i=0; i < s.length(); i++){
+            Set<Integer> set = new HashSet<Integer>();
+            for (int j=0;)
+        }
+    }
+}
